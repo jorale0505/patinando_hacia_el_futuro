453e08abf5ce4220977de60f74fd2c3c594074b1
ingenieria <ingenieria.sintecol@gmail.com>
2026-09-12 00:06:09 -0500

    feat: config para GitHub Pages con index.html y GitHub Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jorale0505.github.io',
+  base: '/patinando_hacia_el_futuro',
   output: 'static',
   build: {
     format: 'directory'
